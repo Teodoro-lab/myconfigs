0409a0d01d5167f932d76e42a5ee1b1044e3b36e
Teo <teodawn.td@gmail.com>
2022-03-05 11:41:40 -0600

    adding install.sh and changes in myphsy

---
+++ installing.sh
@@ -0,0 +1,8 @@
+yay -S 
+yay -S gnome
+yay -S git
+yay -S copyq
+yay -S brave
+sudo wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64 -O /usr/local/bin/oh-my-posh
+sudo chmod +x /usr/local/bin/oh-my-posh
+git clone 
